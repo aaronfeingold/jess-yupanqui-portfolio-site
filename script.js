bce6a3a63356f3fc24d77aab8246661f0345997a
aaronfeingold <ajfeingold88@gmail.com>
2024-07-16 15:37:34 -0400

    script.js function on dom load to pull data and append elements to meta section of header for better SEO

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
+// handle meta data section in the header
 document.addEventListener("DOMContentLoaded", function () {
-  const profileData = {
-    image: "path-to-your-profile-image.jpg",
-    name: "Jess Yupanqui",
-    bio: "Your bio goes here...",
-  };
+  fetch("assets/data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const profileData = data;
+      document.getElementById("page-title").textContent = profileData.logoName;
+      document.getElementById("canonical-link").href =
+        profileData.canonicalLink;
 
-  document.getElementById("profile-image").src = profileData.image;
-  document.getElementById("name").textContent = profileData.name;
-  document.getElementById("bio").textContent = profileData.bio;
+      profileData.metaData.forEach((meta) => {
+        let metaElement = document.createElement("meta");
+
+        if (meta.property) {
+          metaElement.setAttribute("property", meta.property);
+        } else if (meta.itemprop) {
+          metaElement.setAttribute("itemprop", meta.itemprop);
+        } else if (meta.name) {
+          metaElement.setAttribute("name", meta.name);
+        }
+
+        metaElement.content = meta.content;
+        document.head.appendChild(metaElement);
+      });
+    });
 });
